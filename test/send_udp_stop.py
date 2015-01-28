2f07998c758b1978d487303042af091f9ce82184
Simon Cooksey <sjc80@kent.ac.uk>
2015-01-28 17:47:03 +0000

    Only send one packet in UDP python test, rather than infinite

--- test/send_udp_stop.py
+++ test/send_udp_stop.py
@@ -12,5 +12,4 @@ print "message:", MESSAGE
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-while(True) :
-	sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
